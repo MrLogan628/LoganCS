16067159cb3ee862878d00273a966234b8ca5c7f
Sergio Logan <sergiologan1011@gmail.com>
2021-04-25 14:22:37 -0700

    circle.cpp

--- circle.cpp
+++ circle.cpp
@@ -6,10 +6,30 @@ using namespace std;
 
 int main()
 {
-	double radius;
-	cout << "Enter radius\n";
-	//Add code to read user input and store in variable radius
-	cout << "The area of a circle with radius \n";
-	//Calculate the area of the circle
-	return 0;
+    //THE AREA OF A CIRCLE
+
+#include <iostream>
+#include <cmath>
+    using namespace std;
+
+    int main()
+    {
+        double radius;
+        double area;
+        double circumference;
+
+
+        //Step 1 Read radii
+        cout << "Enter Radius: ";
+        cin >> radius;
+
+        //Step 2 Compute Area
+        area = pow(radius, 2) * 3.14159;
+        circumference = 2 * PI * radius;
+        //Step 3 Display the area
+        cout << "The area of the circle is " << area << endl;
+        cout << "The Circumference of the circle is " << circumference << endl;
+        return 0;
+    }
+
 }
